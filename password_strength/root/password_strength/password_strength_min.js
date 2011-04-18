4289e2344db1d5095ddfb57fee844f2a726868e6
Matt <maf675@gmail.com>
2011-04-18 10:37:25 -0700

    Super optimized and lightened up the javascript code.

--- password_strength/root/password_strength/password_strength_min.js
+++ password_strength/root/password_strength/password_strength_min.js
@@ -1,11 +1,11 @@
 /**
 *
 * @package password_strength JavaScript Code mini
-* @version 0.0.4 3/8/11 3:19 PM
+* @version 0.0.5 4/18/11 12:12 AM
 * @copyright (c) 2011 VSE for phpBB
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 * @code inspired by Naked Password by Platform45 at http://www.nakedpassword.com
 *
 **/
 
-jQuery.fn.passwordStrength=function(){return this.each(function(){var fieldColor=$(this).css("background-color");var trigger=function(e){password_level=getPasswordStrength($(this).val());toggleField($(this).attr("id"),password_level)};function toggleField(field,level){switch(level){case 0:$("#"+field).css("background-color",fieldColor);$("#"+field+"text").css("display","none");break;case 1:$("#"+field).css("background-color","#F5A9A9");$("#"+field+"text").html(ps_1).css("display","block");break;case 2:$("#"+field).css("background-color","#F5D0A9");$("#"+field+"text").html(ps_2).css("display","block");break;case 3:$("#"+field).css("background-color","#F3F781");$("#"+field+"text").html(ps_3).css("display","block");break;case 4:$("#"+field).css("background-color","#A9F5A9");$("#"+field+"text").html(ps_4).css("display","block");break;case 5:$("#"+field).css("background-color","#A9F5A9");$("#"+field+"text").html(ps_5).css("display","block");break}}function getPasswordStrength(password){var score=0;if(password.length>5){score++}if((password.match(/[a-z]/))&&(password.match(/[A-Z]/))){score++}if((password.match(/\d+/))&&(password.match(/\D+/))){score++}if((password.match(/[!,@,#,$,%,^,&,*,?,_,~,-,(,)]+/))&&(password.match(/\w+/))){score++}if(password.length>12){score++}return score}$(this).after("<div style='display:none;' id='"+$(this).attr("id")+"text"+"'></div>");$(this).bind('keyup',trigger).bind('blur',trigger)})};$(document).ready(function(){$("#new_password").passwordStrength()});
+jQuery.fn.passwordStrength=function(){return this.each(function(){var fieldColor=new Array($(this).css("background-color"),"#F5A9A9","#F5D0A9","#F3F781","#A9F5A9","#A9F5A9");var respText=new Array("",ps_1,ps_2,ps_3,ps_4,ps_5);function trigger(){var password_score=getPasswordStrength($(this).val());showStrength($(this),password_score)};function showStrength(field,score){field.css("background-color",fieldColor[score]);$("#password_strength").html(respText[score])}function getPasswordStrength(password){return 0+ +(password.length>5)+ +(/[a-z]/.test(password)&&/[A-Z]/.test(password))+ +(/\d/.test(password)&&/\D/.test(password))+ +(/[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/.test(password)&&/\w/.test(password))+ +(password.length>12)}$(this).bind('keyup',trigger).bind('blur',trigger).after("<div id='password_strength'></div>")})};$(document).ready(function(){$("#new_password").passwordStrength()});
