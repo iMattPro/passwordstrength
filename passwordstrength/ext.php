42896a3c1f8f750b590919399f3ac7ab7c8b4f79
Matt Friedman <maf675@gmail.com>
2013-04-10 15:52:55 -0700

    Tweak ext doc block

--- passwordstrength/ext.php
+++ passwordstrength/ext.php
@@ -16,7 +16,7 @@ if (!defined('IN_PHPBB'))
 }
 
 /**
- * Main extension class for passwordstrength extension.
+ * Main extension class for this extension.
  */
 class phpbb_ext_vse_passwordstrength_ext extends phpbb_extension_base
 {
