6d3f099906197d7083a59e1a13257c68180ebfda
Matt Friedman <maf675@gmail.com>
2013-10-01 20:27:07 -0700

    namespace

--- passwordstrength/ext.php
+++ passwordstrength/ext.php
@@ -7,6 +7,8 @@
 *
 */
 
+namespace vse\passwordstrength;
+
 /**
 * @ignore
 */
@@ -18,6 +20,6 @@ if (!defined('IN_PHPBB'))
 /**
 * Main extension class for this extension.
 */
-class phpbb_ext_vse_passwordstrength_ext extends phpbb_extension_base
+class ext extends \phpbb\extension\base
 {
 }
