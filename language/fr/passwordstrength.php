ab78c845fd9928a8323c9f6bcab1b5e366fb1ea2
Raphaël <Galixte@users.noreply.github.com>
2015-01-30 09:25:15 +0100

    Update for FRENCH translation for passwordstrength.php file
    Update for FRENCH translation for passwordstrength.php file

--- language/fr/passwordstrength.php
+++ language/fr/passwordstrength.php
@@ -35,9 +35,9 @@ if (empty($lang) || !is_array($lang))
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
 $lang = array_merge($lang, array(
-	'PS_VERY_WEAK'		=> 'Très faible',
-	'PS_WEAK'			=> 'Faible',
-	'PS_GOOD'			=> 'Correct',
-	'PS_STRONG'			=> 'Complexe',
-	'PS_VERY_STRONG'	=> 'Très complexe',
+	'PS_VERY_WEAK'		=> 'Ce mot de passe est très faible',
+	'PS_WEAK'			=> 'Ce mot de passe est faible',
+	'PS_GOOD'			=> 'Ce mot de passe est correct',
+	'PS_STRONG'			=> 'Ce mot de passe est complexe',
+	'PS_VERY_STRONG'	=> 'Ce mot de passe est très complexe',
 ));
