f1d6eee3b8a8f873de5accb34ba1d686793aba41
Raphaël <Galixte@users.noreply.github.com>
2015-02-04 16:25:32 +0100

    Update for FRENCH translation for Password Strength extension
    Update for FRENCH translation for passwordstrength.php file

--- language/fr/passwordstrength.php
+++ language/fr/passwordstrength.php
@@ -37,7 +37,7 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
 	'PS_VERY_WEAK'		=> 'Très faible',
 	'PS_WEAK'			=> 'Faible',
-	'PS_GOOD'			=> 'Correct',
+	'PS_GOOD'			=> 'Bon',
 	'PS_STRONG'			=> 'Complexe',
 	'PS_VERY_STRONG'	=> 'Très complexe',
 ));
