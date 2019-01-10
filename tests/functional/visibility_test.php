61f9df181b76ba0084d39967369d356690f7ae9c
Matt Friedman <maf675@gmail.com>
2019-01-09 21:44:48 -0800

    Fix tests up [ci skip]

--- tests/functional/visibility_test.php
+++ tests/functional/visibility_test.php
@@ -15,7 +15,7 @@ namespace vse\passwordstrength\tests\functional;
 */
 class visibility_test extends \phpbb_functional_test_case
 {
-	static protected function setup_extensions()
+	protected static function setup_extensions()
 	{
 		return array('vse/passwordstrength');
 	}
