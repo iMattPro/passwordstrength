6c2e954ae5581492c527be6c85489cccc5a28982
Matt Friedman <maf675@gmail.com>
2023-03-31 16:48:05 -0700

    Fix tests
    Signed-off-by: Matt Friedman <maf675@gmail.com>

--- tests/functional/visibility_test.php
+++ tests/functional/visibility_test.php
@@ -41,12 +41,12 @@ class visibility_test extends \phpbb_functional_test_case
 
 		// Test password strength on ACP Account settings page
 		$crawler = self::request('GET', "adm/index.php?i=acp_users&mode=overview&sid=$this->sid");
-		$this->assertNotContainsLang('PS_WEAK', $crawler->filter('html')->text());
+		$this->assertStringNotContainsString('Very\u0020Strong\u0020\uD83D\uDE00', $crawler->filter('html')->text());
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$data = array('username' => 'admin');
 		$form->setValues($data);
 		$crawler = self::submit($form);
-		$this->assertContainsLang('PS_WEAK', $crawler->filter('html')->text());
+		$this->assertStringContainsString('Very\u0020Strong\u0020\uD83D\uDE00', $crawler->filter('html')->text());
 	}
 
 	public function test_ucp_pages()
@@ -55,20 +55,20 @@ class visibility_test extends \phpbb_functional_test_case
 
 		// Test password strength on UCP Account settings page
 		$crawler = self::request('GET', 'ucp.php?i=ucp_profile&mode=reg_details');
-		$this->assertContainsLang('PS_WEAK', $crawler->filter('html')->text());
+		$this->assertStringContainsString('Very\u0020Strong\u0020\uD83D\uDE00', $crawler->filter('html')->text());
 
 		// Test password strength on UCP registration page
 		$this->logout();
 		$crawler = self::request('GET', 'ucp.php?mode=register');
 		$form = $crawler->selectButton($this->lang('AGREE'))->form();
 		$crawler = self::submit($form);
-		$this->assertContainsLang('PS_WEAK', $crawler->filter('html')->text());
+		$this->assertStringContainsString('Very\u0020Strong\u0020\uD83D\uDE00', $crawler->filter('html')->text());
 	}
 
 	public function test_other_pages()
 	{
 		// Test password strength NOT on index page
 		$crawler = self::request('GET', 'index.php');
-		$this->assertNotContainsLang('PS_WEAK', $crawler->filter('html')->text());
+		$this->assertStringNotContainsString('Very\u0020Strong\u0020\uD83D\uDE00', $crawler->filter('html')->text());
 	}
 }
