cf4e2aea58a6ff51591c8efa7629c02a0a2dae2f
Matt Friedman <maf675@gmail.com>
2026-06-21 10:27:44 -0700

    Move language loading from twig to php

--- tests/event/listener_test.php
+++ tests/event/listener_test.php
@@ -57,6 +57,36 @@ class listener_test extends \phpbb_test_case
 		), array_keys(\vse\passwordstrength\event\listener::getSubscribedEvents()));
 	}
 
+	public function get_zxcvbnts_language_data()
+	{
+		return [
+			'default language' => ['en', 'en'],
+			'case insensitive' => ['FR', 'fr'],
+			'phpBB underscore region fallback' => ['en_us', 'en'],
+			'phpBB hyphen region fallback' => ['fr-ca', 'fr'],
+			'German formal alias' => ['de_x_sie', 'de'],
+			'Spanish alias' => ['es', 'es-es'],
+			'Spanish regional fallback' => ['es_ar', 'es-es'],
+			'Croatian Latin alias' => ['hr_x_vi', 'hr'],
+			'Dutch alias' => ['nl', 'nl-be'],
+			'Portuguese alias' => ['pt', 'pt-br'],
+			'Portuguese Brazilian package' => ['pt_br', 'pt-br'],
+			'Simplified Chinese alias' => ['zh_cmn_hans', 'zh'],
+			'Traditional Chinese alias' => ['zh_cmn_hant', 'zh'],
+			'unsupported fallback' => ['xx', 'en'],
+		];
+	}
+
+	/**
+	 * @dataProvider get_zxcvbnts_language_data
+	 */
+	public function test_get_zxcvbnts_language($language, $expected)
+	{
+		$this->set_listener();
+
+		self::assertSame($expected, $this->listener->get_zxcvbnts_language($language));
+	}
+
 	public function password_strength_setup_data()
 	{
 		return array(
@@ -100,7 +130,8 @@ class listener_test extends \phpbb_test_case
 		$dispatcher = new \phpbb\event\dispatcher();
 		$dispatcher->addListener('core.user_setup', array($this->listener, 'password_strength_setup'));
 
-		$event_data = array('lang_set_ext');
+		$user_lang_name = 'en';
+		$event_data = array('lang_set_ext', 'user_lang_name');
 		$event_data_filtered = $dispatcher->trigger_event('core.user_setup', compact($event_data));
 		extract($event_data_filtered, EXTR_OVERWRITE);
 
@@ -108,6 +139,25 @@ class listener_test extends \phpbb_test_case
 		{
 			self::assertContains($expected, $lang_set_ext);
 		}
+	}
+
+	public function test_password_strength_setup_uses_event_language()
+	{
+		$this->set_listener();
+
+		$this->template->expects(self::once())
+			->method('assign_vars')
+			->with(self::callback(function ($vars) {
+				return isset($vars['ZXCVBNTS_LANG']) && $vars['ZXCVBNTS_LANG'] === 'de';
+			}));
+
+		$dispatcher = new \phpbb\event\dispatcher();
+		$dispatcher->addListener('core.user_setup', array($this->listener, 'password_strength_setup'));
+
+		$lang_set_ext = array();
+		$user_lang_name = 'de_x_sie';
+		$event_data = array('lang_set_ext', 'user_lang_name');
+		$dispatcher->trigger_event('core.user_setup', compact($event_data));
 	}
 
 	public function password_strength_acp_options_data()
